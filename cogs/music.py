7c0aa7379f61e57b773b5f17dfce9f00c36ceb8c
RajbirBains <rajbir.s.bains@gmail.com>
2025-01-15 21:46:35 -0800

    Added ping command + embedded msgs

--- cogs/music.py
+++ cogs/music.py
@@ -1,3 +1,19 @@
 import discord
 from discord.ext import commands
 import json, requests
+
+
+class PlayMusic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Command PlayMusic is ready")
+
+    @commands.command()
+    async def play(self, ctx, *args): # *args allows for multiple/no arguments
+        await ctx.send(f'Filler for play')
+
+async def setup(bot):
+    await bot.add_cog(PlayMusic(bot))
--- cogs/test.py
+++ cogs/test.py
@@ -12,7 +12,16 @@ class Test(commands.Cog):
     
     @commands.command()
     async def test(self, ctx):
-        await ctx.send("Testing")
+        embedded_msg = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+        embedded_msg.add_field(name="Field1", value="testing field 1", inline=False)
+        await ctx.send(embed = embedded_msg)
+
+    @commands.command()
+    async def ping(self, ctx):
+        embedded_msg_ping = discord.Embed(title="Bot Ping", description="Latency of bot (ms)", color= discord.Color.blue())
+        embedded_msg_ping.add_field(name=f"{self.bot.user.name} latency (ms): ", value= f"{round(self.bot.latency * 1000)} ms" )
+        embedded_msg_ping.set_footer(text=f"Requested by {ctx.author.name}", icon_url= ctx.author.avatar)
+        await ctx.send(embed = embedded_msg_ping)
 
 async def setup(bot):
     await bot.add_cog(Test(bot))
